1e36cce2f3618ba2d9be23d04fa29966965cb742
dyu <david.yu.ftw@gmail.com>
2017-02-22 19:36:40 +0800

    formatting

--- all.sh
+++ all.sh
@@ -5,13 +5,16 @@ CURRENT_DIR=$PWD
 rm -rf meson-out bfg9000-out cmake-out buck-out
 
 # meson
-mkdir meson-out && cd meson-out && meson .. --buildtype release && ninja && ninja test && \
+mkdir meson-out && cd meson-out && \
+    meson .. --buildtype release && \
+    ninja && ninja test && \
     ./executable
 
 cd $CURRENT_DIR
 
 # bfg9000
-9k bfg9000-out && cd bfg9000-out && ninja && ninja test && \
+9k bfg9000-out && cd bfg9000-out && \
+    ninja && ninja test && \
     ./executable
 
 cd $CURRENT_DIR
